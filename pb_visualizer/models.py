f49b77318705f2ed253e815c9bd46e75058f0988
Simon Rey <reysimon@orange.fr>
2023-09-18 12:09:18 +0200

    add_election fix

--- pb_visualizer/models.py
+++ pb_visualizer/models.py
@@ -371,7 +371,7 @@ class RuleResult(models.Model):
 
 
 class RuleResultMetadata(models.Model):
-    name = models.CharField(max_length=50,
+    name = models.CharField(max_length=100,
                             unique=True)
     short_name = models.CharField(max_length=15,
                                   unique=True,
