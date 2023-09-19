2958d99f6e1db87d6142edfcbafaa81d3f862612
Simon Rey <reysimon@orange.fr>
2023-09-19 17:22:04 +0200

    Beginning on splitting compute_properties

--- pb_visualizer/management/commands/compute_properties.py
+++ pb_visualizer/management/commands/compute_properties.py
@@ -1,115 +1,16 @@
-from importlib import metadata
-import time
 import json
 from typing import Iterable
-from django.db.models import Model
-from django.core.management.base import BaseCommand
-import numpy as np
 
-from pb_visualizer.pabutools import project_object_to_pabutools, election_object_to_pabutools
-from pb_visualizer.models import *
-from pabutools import election as pbinstance
-from pabutools import rules
-from pabutools.analysis import instanceproperties, profileproperties, category, votersatisfaction, satisfaction_histogram
-from pabutools.election.satisfaction import CC_Sat, Cost_Sat, Cardinality_Sat, Effort_Sat, Relative_Cardinality_Sat, Relative_Cost_Sat, Relative_Cost_Approx_Normaliser_Sat
-from pabutools.election.instance import max_budget_allocation_cardinality, max_budget_allocation_cost
 import pabutools.fractions as fractions
+from django.core.management.base import BaseCommand
+from pabutools.analysis import instanceproperties, category, votersatisfaction, satisfaction_histogram
+from pabutools.election.instance import max_budget_allocation_cost
+from pabutools.election.satisfaction import Cost_Sat
 
-
-instance_property_mapping = {
-    "sum_proj_cost": instanceproperties.sum_project_cost,
-    "fund_scarc": instanceproperties.funding_scarcity,
-    "avg_proj_cost": instanceproperties.avg_project_cost,
-    "med_proj_cost": instanceproperties.median_project_cost,
-    "sd_proj_cost": instanceproperties.std_dev_project_cost
-}
-
-profile_property_mapping = {
-    "avg_ballot_len": profileproperties.avg_ballot_length, 
-    "med_ballot_len": profileproperties.median_ballot_length, 
-    "avg_ballot_cost": profileproperties.avg_ballot_cost, 
-    "med_ballot_cost": profileproperties.median_ballot_cost, 
-    "avg_app_score":  profileproperties.avg_approval_score,
-    "med_app_score":  profileproperties.median_approval_score,
-    "avg_total_score": profileproperties.avg_total_score, 
-    "med_total_score": profileproperties.median_total_score, 
-}
-
-satisfaction_property_mapping = {
-    "avg_card_sat": {'sat_class': Cardinality_Sat, 'normalizer_func': None},
-    "avg_cost_sat": {'sat_class': Cost_Sat, 'normalizer_func': None},
-    "avg_nrmcard_sat": {'sat_class': Cardinality_Sat, 'normalizer_func': max_budget_allocation_cardinality},
-    "avg_nrmcost_sat": {'sat_class': Cost_Sat, 'normalizer_func': max_budget_allocation_cost},
-    "avg_relcard_sat": {'sat_class': Relative_Cardinality_Sat, 'normalizer_func': None},
-    "avg_relcost_sat": {'sat_class': Relative_Cost_Approx_Normaliser_Sat, 'normalizer_func': None},
-    "happiness": {'sat_class': CC_Sat, 'normalizer_func': None},
-}
-
-gini_property_mapping = {
-    "equality": Cost_Sat,
-    # "fairness": Effort_Sat,
-}
-
-def rule_mapping(budget):
-    return {
-        'greedy_card': {'func': rules.greedy_utilitarian_welfare, 'params': {'sat_class': Cardinality_Sat}},
-        'greedy_cost': {'func': rules.greedy_utilitarian_welfare, 'params': {'sat_class': Cost_Sat}},
-        'greedy_cc': {'func': rules.greedy_utilitarian_welfare, 'params': {'sat_class': CC_Sat}},
-        'max_card': {'func': rules.max_additive_utilitarian_welfare, 'params': {'sat_class': Cardinality_Sat}},
-        'max_cost': {'func': rules.max_additive_utilitarian_welfare, 'params': {'sat_class': Cost_Sat}},
-        'mes_uncompleted': {'func': rules.method_of_equal_shares, 'params': {'sat_class': Cost_Sat}},
-        'mes': {'func': rules.completion_by_rule_combination, 'params': {
-            'rule_sequence': [rules.exhaustion_by_budget_increase, rules.greedy_utilitarian_welfare],
-            'rule_params': [
-                {
-                    'rule': rules.method_of_equal_shares, 
-                    'rule_params': {'sat_class': Cost_Sat}, 
-                    'budget_step': float(budget)/100
-                },
-                {"sat_class": Cost_Sat},
-            ]
-        }},
-        'mes_greedy_cost': {'func': rules.completion_by_rule_combination, 'params': {
-            'rule_sequence': [rules.method_of_equal_shares, rules.greedy_utilitarian_welfare],
-            'rule_params': [{'sat_class': Cost_Sat}, {'sat_class': Cost_Sat}]
-        }},
-        'seq_phragmen': {'func': rules.sequential_phragmen, 'params': {}}
-    }
-    
-
-
-def print_if_verbose(string, required_verbosity=1, verbosity=1., persist=False):
-    if verbosity < required_verbosity:
-        return
-    elif verbosity == required_verbosity and not persist:
-        print(string.ljust(80), end="\r")
-    else:
-        print(string.ljust(80))
-
-
-def exists_in_database(model_class: type[Model],
-                       **filters):
-    query = model_class.objects.filter(**filters)
-    return query.exists()
-
-
-class LazyElectionParser():
-    def __init__(self, election_obj: Election, verbosity: int = 1):
-        self.election_obj = election_obj
-        self.instance = None
-        self.profile = None
-        self.verbosity = verbosity
-    
-    def get_election_obj(self):
-        return self.election_obj
-
-    def get_parsed_election(self):
-        if not self.instance or not self.profile:
-            print_if_verbose("translating model...", 1, self.verbosity)
-            self.instance, self.profile = election_object_to_pabutools(self.election_obj)
-        return self.instance, self.profile
-        
-    
+from pb_visualizer.management.commands.utils import LazyElectionParser, instance_property_mapping, exists_in_database, \
+    profile_property_mapping, rule_mapping, print_if_verbose, satisfaction_property_mapping, gini_property_mapping
+from pb_visualizer.models import *
+from pb_visualizer.pabutools import project_object_to_pabutools
 
 
 def compute_election_properties(election_parser: LazyElectionParser,
@@ -127,9 +28,11 @@ def compute_election_properties(election_parser: LazyElectionParser,
             if overwrite or not exists_in_database(ElectionDataProperty, **unique_filters):
                 instance, profile = election_parser.get_parsed_election()
                 ElectionDataProperty.objects.update_or_create(**unique_filters,
-                                                              defaults={"value": instance_property_mapping[instance_property](instance)}) 
+                                                              defaults={
+                                                                  "value": instance_property_mapping[instance_property](
+                                                                      instance)})
 
-    # we now compute the profile properties
+                # we now compute the profile properties
     for profile_property in profile_property_mapping:
         metadata_obj = ElectionMetadata.objects.get(short_name=profile_property)
         if metadata_obj.applies_to_election(election_obj):
@@ -138,7 +41,9 @@ def compute_election_properties(election_parser: LazyElectionParser,
             if overwrite or not exists_in_database(ElectionDataProperty, **unique_filters):
                 instance, profile = election_parser.get_parsed_election()
                 ElectionDataProperty.objects.update_or_create(**unique_filters,
-                                                              defaults={"value": profile_property_mapping[profile_property](instance, profile)}) 
+                                                              defaults={
+                                                                  "value": profile_property_mapping[profile_property](
+                                                                      instance, profile)})
 
 
 def compute_election_results(election_parser: LazyElectionParser,
@@ -165,7 +70,8 @@ def compute_election_results(election_parser: LazyElectionParser,
                         **rules[rule]['params']
                     )
                     rule_result_obj.selected_projects.set(
-                        [Project.objects.get(election=election_obj, project_id=project.name) for project in pabutools_result]
+                        [Project.objects.get(election=election_obj, project_id=project.name) for project in
+                         pabutools_result]
                     )
 
 
@@ -177,8 +83,10 @@ def compute_rule_result_properties(election_parser: LazyElectionParser,
     election_obj = election_parser.get_election_obj()
 
     for rule_result_object in RuleResult.objects.filter(election=election_obj):
-        print_if_verbose("Computing properties for {} results.".format(rule_result_object.rule.abbreviation), 2, verbosity)
-        budget_allocation = [project_object_to_pabutools(project) for project in rule_result_object.selected_projects.all()]
+        print_if_verbose("Computing properties for {} results.".format(rule_result_object.rule.abbreviation), 2,
+                         verbosity)
+        budget_allocation = [project_object_to_pabutools(project) for project in
+                             rule_result_object.selected_projects.all()]
 
         for property in satisfaction_property_mapping:
             if rule_property_list == None or property in rule_property_list:
@@ -197,10 +105,12 @@ def compute_rule_result_properties(election_parser: LazyElectionParser,
                         )
                         normalizer = 1
                         if satisfaction_property_mapping[property]['normalizer_func'] != None:
-                            normalizer = satisfaction_property_mapping[property]['normalizer_func'](instance, instance.budget_limit)
-                            
+                            normalizer = satisfaction_property_mapping[property]['normalizer_func'](instance,
+                                                                                                    instance.budget_limit)
+
                         RuleResultDataProperty.objects.update_or_create(**unique_filters,
-                                                                        defaults={"value": str(float(value/normalizer))}) 
+                                                                        defaults={
+                                                                            "value": str(float(value / normalizer))})
 
         for property in gini_property_mapping:
             if rule_property_list == None or property in rule_property_list:
@@ -222,7 +132,7 @@ def compute_rule_result_properties(election_parser: LazyElectionParser,
                                                                         defaults={"value": str(float(value))})
 
         property = "category_prop"
-        if rule_property_list == None or property in rule_property_list:
+        if rule_property_list is None or property in rule_property_list:
             metadata_obj = RuleResultMetadata.objects.get(short_name=property)
             if metadata_obj.applies_to_election(election_obj) and election_obj.has_categories:
                 unique_filters = {'rule_result': rule_result_object,
@@ -236,11 +146,11 @@ def compute_rule_result_properties(election_parser: LazyElectionParser,
                         budget_allocation
                     )
                     RuleResultDataProperty.objects.update_or_create(**unique_filters,
-                                                                    defaults={"value": str(float(value))}) 
-    
+                                                                    defaults={"value": str(float(value))})
+
         if len(budget_allocation) > 0:
             property = "med_select_cost"
-            if rule_property_list == None or property in rule_property_list:
+            if rule_property_list is None or property in rule_property_list:
                 metadata_obj = RuleResultMetadata.objects.get(short_name=property)
                 if metadata_obj.applies_to_election(election_obj):
                     unique_filters = {'rule_result': rule_result_object,
@@ -249,10 +159,10 @@ def compute_rule_result_properties(election_parser: LazyElectionParser,
                         print_if_verbose("Computing {}.".format(property), 3, verbosity)
                         value = instanceproperties.median_project_cost(budget_allocation)
                         RuleResultDataProperty.objects.update_or_create(**unique_filters,
-                                                                        defaults={"value": str(float(value))}) 
+                                                                        defaults={"value": str(float(value))})
 
         property = "agg_nrmcost_sat"
-        if rule_property_list == None or property in rule_property_list:    
+        if rule_property_list is None or property in rule_property_list:
             metadata_obj = RuleResultMetadata.objects.get(short_name=property)
             if metadata_obj.applies_to_election(election_obj):
                 unique_filters = {'rule_result': rule_result_object,
@@ -272,7 +182,6 @@ def compute_rule_result_properties(election_parser: LazyElectionParser,
                                                                     defaults={"value": json.dumps(value)})
 
 
-
 def compute_properties(election_names=None,
                        rules=None,
                        rule_properties=False,
@@ -286,25 +195,27 @@ def compute_properties(election_names=None,
         fractions.FRACTION = 'float'
     n_elections = len(election_query)
     for index, election_obj in enumerate(election_query):
-        print_if_verbose("Computing properties of election {}/{}: {}".format(index+1, n_elections, election_obj.name), 1, verbosity, persist=True)
+        print_if_verbose("Computing properties of election {}/{}: {}".format(index + 1, n_elections, election_obj.name),
+                         1, verbosity, persist=True)
         election_parser = LazyElectionParser(election_obj, verbosity)
-        
+
         print_if_verbose("basic election properties...", 1, verbosity)
         compute_election_properties(election_parser, overwrite=overwrite, verbosity=verbosity)
 
         if rules == None or len(rules) > 0:
             print_if_verbose("rule results...", 1, verbosity)
             compute_election_results(election_parser, rules, overwrite=overwrite, verbosity=verbosity)
-        
+
         if rule_properties == None or len(rule_properties) > 0:
             print_if_verbose("rule result properties...", 1, verbosity)
             compute_rule_result_properties(election_parser, rule_properties, overwrite=overwrite, verbosity=verbosity)
 
+
 class Command(BaseCommand):
     help = "compute all properties, rules and rule result properties for the elections in the database"
-    
+
     def add_arguments(self, parser):
-        parser.add_argument('-e','--election_names', nargs='*', type=str, default=None,
+        parser.add_argument('-e', '--election_names', nargs='*', type=str, default=None,
                             help="Give a list of election names for which you want to compute properties.")
         parser.add_argument('-r', '--rules', nargs='*', type=str, default=None,
                             help="Give a list of rules for which you want to the results. Discard option to compute all rules, no parameter to skip the rule computation.")
@@ -314,8 +225,7 @@ class Command(BaseCommand):
                             help="Use exact fractions instead of floats for computing results.")
         parser.add_argument('-o', '--overwrite', nargs='?', type=bool, const=True, default=False,
                             help="Overwrite properties that were already computed.")
-        # parser.add_argument('-o', '--overwrite', nargs='?', type=bool, const=True, default=False)
-    
+
     def handle(self, *args, **options):
         compute_properties(election_names=options['election_names'],
                            rules=options['rules'],
---
+++ pb_visualizer/management/commands/export_model.py
@@ -0,0 +1,27 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from django.apps import apps
+
+
+class Command(BaseCommand):
+    help = "Write a given model into a CSV file"
+
+    def add_arguments(self, parser):
+        parser.add_argument('-m', nargs='+', type=str, help='the models to be exported')
+        parser.add_argument('-f', nargs='?', default="", type=str, help='the prefix for the file path')
+
+    def handle(self, *args, **options):
+        if not options['m']:
+            print("ERROR: you need to pass a model as input using arugment -m <MODEL_NAME>.")
+            return
+        for model_name in options['m']:
+            model = apps.get_model(app_label='pb_visualizer', model_name=model_name)
+            file_prefix = options['f'] + ("_" if len(options['f']) > 0 else "")
+            with open(f"{file_prefix}{model_name}.csv", "w") as csv_file:
+                writer = csv.writer(csv_file)
+                field_names = [field.name for field in model._meta.get_fields()]
+                writer.writerow(field_names)
+                for obj in model.objects.all():
+                    writer.writerow([getattr(obj, field) for field in field_names])
